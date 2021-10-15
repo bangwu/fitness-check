916a70e94b6d34d70c500d3c54fb1548ba63e30d
GitHub Action <action@github.com>
2021-10-15 08:33:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/bangwu/fitnesscheck/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bangwu/fitnesscheck/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bangwu.fitnesscheck.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bangwu.fitnesscheck.MyBundle
 
 class MyProjectService(project: Project) {
 
